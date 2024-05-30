08e876c82cb10ba092ea77953142f0897113a1e7
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-05-30 18:47:45 +0500

    js

---
+++ function.js
@@ -0,0 +1,13 @@
+function myname(name) {
+    console.log("my name is abdul rehman");
+    console.log(`my name is ${name} hello   `);
+    
+}
+myname("hunny");
+
+
+function slam() {
+    return "Hello bw muslims";
+}
+var greeting = slam();
+console.log(greeting);
