657609a69277b0143791c10466c8677c4f18de8b
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-06-03 17:57:20 +0500

    array in js

---
+++ array.js
@@ -0,0 +1,15 @@
+var cities = ["chakwal", "islamabad","jhelum","multan"];
+console.log(cities);
+console.log(cities.length);
+var countries = new Array("pakistan","saudi","dubai", "qatar");
+console.log(countries[3]);
+countries[3] = "china";
+console.log(countries);
+var user = new Array("hunny", "hunny@gmail", 2,34,true);
+console.log(user);
+user.pop();
+user.pop();
+user.pop();
+user.pop();
+
+console.log(user);
