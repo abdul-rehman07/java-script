4e6301d6711cfa574c51dbd1991ab212e80ba9be
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-06-06 18:52:07 +0500

    fill and filter and sliceand splicer

---
+++ fill&filter.js
@@ -0,0 +1,5 @@
+var  mynumber = [2,4,6,1,4,7,9,2];
+console.log(mynumber.fill("h",1,3));
+var counting = [23,24,25,66,77,88,99];
+var result = counting.filter( (num) => num > 23 );
+console.log(result);
---
+++ slice&splice.js
@@ -0,0 +1,4 @@
+var values = [ "Ted","Bed "," Red " , "Sad", "Mad","Fat"];
+// console.log(values.slice(1,3,));
+values.splice(1,5,"hunny");
+console.log(values);
