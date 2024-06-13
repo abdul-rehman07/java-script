f02f44912d682c10733fb911978487fc59b419a6
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-06-13 18:32:55 +0500

    programs

---
+++ scope&literal.js
@@ -0,0 +1,20 @@
+// scopes in js
+
+// var name = "blocker";
+// if (true) {
+//   let fullname = "buster";
+// }
+// console.log(fullname);
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+// }
+
+// literal in js
+var name = `code's`;
+
+var name2 = 'name"s';
+
+var fullname = `${name2} name3`;
+console.log(fullname);
+console.log(fullname.charAt(0));
+console.log(fullname.includes("name3"));
