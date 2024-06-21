a4338a2254fd9728e4126c6bff1e453cc24b7705
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-06-21 15:57:36 +0500

    pending

--- APIhandle.js
+++ APIhandle.js
@@ -1,9 +1,25 @@
-fetch("https://api.chucknorris.io/jokes/random/")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    var prog = data.value;
-    console.log(prog);
-  })
-  .catch();
+// fetch("https://api.chucknorris.io/jokes/random/")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     var prog = data.value;
+//     console.log(prog);
+//   })
+//   .catch();
+
+async function getResponse() {
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/random/");
+
+    const data = await response.json();
+    console.log(data.value);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+getResponse();
+// .then((data) => {
+//   console.log("Random Chuck Norris joke:", data.value);
+// })
+// .catch();
